9e6a9b2b68dd56efa8111afa9f329d877b53cd9d
Daksh goel <123328399+Daksh-Goel12@users.noreply.github.com>
2023-12-26 06:31:20 +0000

    Update images

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/office-desk.avif";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "office desk with lamp";
 
 /**
  * Project list
