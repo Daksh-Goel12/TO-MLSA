61d5770c62af483c7076db9e190a86228ea458f9
Daksh goel <123328399+Daksh-Goel12@users.noreply.github.com>
2023-12-26 06:14:35 +0000

    Update "About me" and social accounts

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Daksh Goel",
+  title: "Web Developer & App Developer",
+  email: "daksh@example.com",
+  gitHub: "Daksh-Goel12",
+  instagram: "Daksh-goel",
+  linkedIn: "Daksh Goel",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
